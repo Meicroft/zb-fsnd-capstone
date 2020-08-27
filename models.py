0b35be75efa0e9dcc16845c70389f43b6187815a
Zach Brown <zach.a.brown@icloud.com>
2020-08-27 15:32:47 -0400

    copy models.py over from previous builds attempt

---
+++ models.py
@@ -0,0 +1,100 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Date
+from flask_migrate import Migrate
+
+# ---------
+# Config.
+# ---------
+
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "agency"
+    database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+# ---------
+# Models
+# ---------
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(180), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f"<Movie id='{self.id}' name='{self.title}'>"
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date
+        }
+
+
+class Actor(db.Model):
+    __tablename__ = "actors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(180), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f"<Actor id='{self.id}' name='{self.name}'>"
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender
+        }
