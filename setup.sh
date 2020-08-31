f8a41ff027c095cc7492b007e7fa87806db5ef0a
Zach Brown <zach.a.brown@icloud.com>
2020-08-31 09:39:53 -0400

    reset back to last working state

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# export DATABASE_URL=postgres://xmcfezduvcwups:cfef78afa0f528bb54e13813fa8aaac4e518b303c9edf331a5f388ba87e7bf80@ec2-54-243-67-199.compute-1.amazonaws.com:5432/de5n0g9e1fa0r5
+export DATABASE_URL=postgres://xmcfezduvcwups:cfef78afa0f528bb54e13813fa8aaac4e518b303c9edf331a5f388ba87e7bf80@ec2-54-243-67-199.compute-1.amazonaws.com:5432/de5n0g9e1fa0r5
 export AUTH0_DOMAIN='dev-71tt1j9b.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE=casting
